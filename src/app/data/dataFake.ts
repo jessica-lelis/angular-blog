c03fdb7fd4a99fa78444fe3024b94fb4a2aa139b
lelis <lelis@policiamilitar.sp.gov.be>
2024-02-15 18:42:48 -0300

    ajustando links e imagens do john wick blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,30 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "JOHN WICK – (2014)",
+    "description": "John Wick é um homem como qualquer um: está casado, tem uma bela casa e consideravelmente feliz." +
+    "Entretanto tal situação advém de uma escolha a qual teve como parâmetro tanto a vida sombria do passado quanto o que veio à transformar sua vida."+
+    "Seu passado é basicamente uma mancha de sangue, é nebuloso, triste e soturno. John, interpretado por Keanu Reeves, é um matador profissional que trabalhava junto à mafia russa. Ao encontrar com alguém especial, sua vida de embates e de guerra foi esquecida, e o novo bem estar conjugal abasteceu sua vitalidade de esperança em ter um recomeço ou uma nova história. Ao ser assaltado pelo filho de um mafioso poderoso com o qual John antigamente trabalhava, e ainda ter que passar por um outro processo de luto diante das ocorrências ali situadas, seu emocional e seu instinto matador ganha potência, e o ódio e a raiva que naquele corpo estavam dormindo por alguns anos revivem, e com mais força.",
+    "photoCover":"https://cinemacao.com/wp-content/uploads/2016/01/John-Wick-1-Wallpaper.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "John Wick: Um Novo Dia para Matar – (2017)",
+    "description": "John Wick é forçado a deixar a aposentadoria mais uma vez por causa de uma promessa antiga e viaja para Roma," +
+    "com o objetivo de ajudar um velho amigo a derrubar uma organização secreta, perigosa e mortal de assassinos procurados em todo o mundo.",
+    "photoCover":"https://www.magazine-hd.com/apps/wp/wp-content/uploads/2017/02/john-wick-2-01.jpg"
+  },
+  {
+    "id":"3",
+    "title": "John Wick 3: Parabellum – (2019)",
+    "description": "O terceiro capítulo da saga segue o lendário John Wick (Reeves) na luta para sair de Nova York, nos Estados Unidos, quando um contrato de 14 milhões de dólares faz dele o alvo dos maiores assassinos do mundo." +
+    "Na trama, após assassinar o chefe da máfia Santino D’Antonio (Riccardo Scamarcio), Wick passa a ser perseguido pelos membros da Alta Cúpula. Então, ele precisa unir forças com antigos parceiros enquanto luta por sua sobrevivência.",
+    "photoCover":"https://images.hdqwalls.com/wallpapers/john-wick-3-4k-dx.jpg"
+  },
+  {
+    "id":"4",
+    "title": "John Wick 4: Baba Yaga – (2023)",
+    "description": "O ex-assassino de aluguel John Wick é procurado pelo mundo todo e a recompensa por sua captura aumenta cada vez mais.",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/01/john_wick_4__j2fsgi-1210x544.jpg"
   }
 ]
